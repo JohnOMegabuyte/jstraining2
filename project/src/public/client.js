f5b18e55a041a72191b39d20c5f621471849c458
John Onah <john.onah@megabuyte.com>
2022-02-20 17:52:58 +0000

    commit after all requirements met

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,33 +1,197 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
+console.log(`insiiide`);
+const originalState = {
+    user: { name: "Udacity" },
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
+    // roverSol:{Curiosity:},
+    selectedRover: undefined,
+    showing: `root`,
 }
+const showingOpts = {
+    missionDetails: `missionDetails`,
+    latestPics: `latestPics`,
+    // aboutRover: `aboutRover`,
 
+}
+const showingOptsDisplay = {
+    latestPics: `Latest Images`,
+    missionDetails: `Mission Details`,
+    // aboutRover: `About Rover`,
+
+}
+const history = []; // states stored in history const to  enable nav
+const HISTORY_DEPTH = 15; //how many backs
+
+function getCurrentState() {
+    return getHistory(history.length - 1);
+}
+function addtoHistory(prev, current) {
+    console.log(`updating history`, prev, current);
+    if (history.size == HISTORY_DEPTH) {
+        history = history.splice(0, 1);
+    }
+    history.push(Object.assign({}, current));//new object added
+    navigation(history.length - 1, true); //update nav page to latest index
+
+}
+/**
+ * get a page from index, stored in persistent nav history
+ * @param {*} index 
+ * @returns 
+ */
+function getHistory(index) {
+    if (history.length < index || index < 0) {
+        console.log(`History Out Of Bounds`);
+        return {};
+    }
+    return history[index];
+
+}
+/**
+ * high order fn for navigating
+ * @returns a navigation function with a hidden state
+ */
+function navigator() {
+    let currPage = 0;
+    return function (val, newPage) {
+        if (newPage) {
+            console.log(`new page navigation now ${val}`);
+            currPage = val;
+        }
+
+        else {
+            let temp = currPage + val;
+            console.log(temp);
+            if (temp < history.length && temp >= 0) {
+                currPage += val;
+                App(getHistory(currPage), true);
+            }
+        }
+
+    }
+}
+//call oon back and fwd button
+function goBack() {
+    navigation(-1);
+
+}
+function goForward() {
+    navigation(1);
+}
+const navigation = navigator();
+
+
+const elements = document.getElementsByClassName("togglediv");
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
+const roverPage = document.getElementById('rover');
+const loading = document.getElementById('loading');
+const greeting = document.getElementById('greeting');
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+
+// called every new page navigation except back buttons
+const updateStore = (store, changes, fromNavigator) => {
+    const changed = Object.assign({}, store, changes);//assign to new object, dont change others
+    if (!fromNavigator)
+        addtoHistory(store, changed);
+    return changed;
+}
+/**
+ * call after loading fnished, set relevant innerhtml vals
+ */
+const draw = (val) => {
+    const state = getCurrentState();
+    // console.log(`Calling Draw`, root, val, state);
+    switch (state.showing) {
+        case `root`:
+            root.innerHTML = val;
+            hideLoading(`root`);
+
+            break;
+        default:
+            roverPage.innerHTML = val;
+            hideLoading(`roverMain`);
+
+            break;
+    }
+    // hideLoading(state.showing);
+
+
+}
+//show or hide loading div
+function showLoading() {
+    console.log(`showing load`);
+
+    Array.from(elements).forEach(function (element) {
+        if (element.id != `loading`) {
+            element.style.display = `none`;
+        } else {
+            element.style.display = `block`;
+        }
+    });
+}
+function hideLoading(toShow) {
+    console.log(`hiding load`, toShow);
+
+    Array.from(elements).forEach(function (element) {
+        if (element.id != toShow) {
+            element.style.display = `none`;
+        } else {
+            console.log(element);
+            element.style.display = `block`;
+        }
+    });
+}
+function setShowing(toSet) {
+    App({ showing: toSet });
+}
+function buildRoverPageOpts() {
+    return `<div class="row col-lg-12" style="display: inline; justify-content: space-between;">` + Object.keys(showingOpts).map(opt => {
+        return ` <button class="btn btn-default" onclick="setShowing(&quot;${opt}&quot;)">${showingOptsDisplay[opt]}</button>`;
+
+    }).join(`&nbsp&nbsp`) + `</div>`;
 }
 
-const render = async (root, state) => {
-    root.innerHTML = App(state)
+function selectRover(name) {
+    console.log(`Rover Selected ${name}`)
+    // showing = showingOpts.latestPics;//todo delete
+    App({ selectedRover: name, showing: showingOpts.missionDetails });
 }
-
-
+function getRoverListButtons() {
+    return originalState.rovers.map(rover => {
+        return `
+        <button  onclick="selectRover(&quot;${rover}&quot;)">${rover}</button>
+      `;
+    }).join(``);
+}
+function getRoversList() {
+    return `<div class="row col-lg-12" style="display: inline; justify-content: space-between;  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);">
+   <h3>Select A Rover To View Latest Images And Data</h3>
+  
+   `+ getRoverListButtons() + `</div>`;
+}
+function goHome() {
+    App(originalState);
+}
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
+const App = (state, fromNavigator) => {
+    console.log(`loading with update`, state);
+    showLoading();
+    let currentState = updateStore(getCurrentState(), state, fromNavigator);//only update history if not navigator arrows
+    let { rovers, selectedRover, showing } = currentState;
+    greeting.innerHTML = Greeting(selectedRover);
 
-    return `
-        <header></header>
+    if (selectedRover) {
+        getRoverPage(showing, selectedRover);
+        // roverPage
+    }
+    else {
+        return getImageOfTheDay(`
         <main>
-            ${Greeting(store.user.name)}
+        ${getRoversList()}
+
+           
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
+                <h3>Astronomy Picture Of The Day</h3>
                 <p>
                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
@@ -36,26 +200,30 @@ const App = (state) => {
                     explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
                     but generally help with discoverability of relevant imagery.
                 </p>
-                ${ImageOfTheDay(apod)}
+                %%replace%%
             </section>
         </main>
         <footer></footer>
-    `
+    `)
+    }
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
-
+    App(originalState);
+});
 // ------------------------------------------------------  COMPONENTS
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
+const Greeting = (selectedRover) => {
+    // ${Greeting(originalState.user.name)}
+    console.log(selectedRover);
+    let l2 = (selectedRover != undefined ? `<div"> <h5>Viewing Rover: ${selectedRover}</h5> <div>` : ``);
+    let name = originalState.user.name;
     if (name) {
         return `
-            <h1>Welcome, ${name}!</h1>
-        `
+            <h3>Welcome, ${name}!</h3>
+        `+ l2;
     }
 
     return `
@@ -63,43 +231,99 @@ const Greeting = (name) => {
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+// ------------------------------------------------------  API CALLS
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
+// Removed circular calls, now draw once on return, updates when refreshed
+const getImageOfTheDay = (textValue) => {
+    let ret = ``;
+    fetch(`http://localhost:3000/apod`)
+        .then(res => res.json())//turn response to json
+        .then(apod => {
+            // check if the photo of the day is actually type video!
+            if (apod.media_type === "video") {
+                ret += `
             <p>See today's featured video <a href="${apod.url}">here</a></p>
             <p>${apod.title}</p>
             <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
+        `
+            } else {
+                ret += `
+            <img src="${apod.image.url}" height="350px" width="100%" /> 
             <p>${apod.image.explanation}</p>
-        `)
-    }
+        `;
+            }
+            textValue = textValue.replace(`%%replace%%`, ret);
+            draw(textValue);
+
+        })
+
+    return undefined;
+}
+function constructDatedImage(dimg) {
+    return `<div padding-right:3% style="display:block; width:20%;" >
+        <img style="display:block;
+        height: auto;
+       width: 100%;
+       " src="${dimg.img_src}" >
+        <br>
+        <span> ${dimg.earth_date}</span>
+    
+    </div>&nbsp; &nbsp; &nbsp;`;
+}
+function buildRoverImageDisplays(ret, datedImages) {
+    console.log(datedImages);
+    ret += `<div class ="row col-lg-12
+    " style=" display: flex;
+    margin: 5em;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    flex-wrap: wrap;">`;
+
+    ret += datedImages.map(dm => constructDatedImage(dm)).join(``);
+
+    ret += `</div>`;
+    return ret;
+}
+function buildRoverDetailsDisplay(ret, displayObj) {
+    
+    ret+= `<div style ="text-align: center;
+   ">`;
+    
+    ret += Object.keys(displayObj).map(head=>{
+        return `<h5><b>${head.replace(`_`,` `).toUpperCase()}</b>: ${displayObj[head]}   </h5>`
+    }).join(``);
+    
+    ret += `</div>`;
+    return ret;
 }
 
-// ------------------------------------------------------  API CALLS
+function getRoverPage(showing, selectedRover) {
+    let ret = buildRoverPageOpts();
+    console.log(`showww`, showing);
+    switch (showing) {
+        case showingOpts.latestPics:
+            fetch(`http://localhost:3000/rover/latestPics?selectedRover=${selectedRover}`)
+                .then(res => res.json())//turn response to json
+                .then(datedImages => {
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+                    ret = buildRoverImageDisplays(ret, datedImages.images);
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+                    draw(ret);
 
-    return data
+                })
+            break;
+        case showingOpts.missionDetails:
+            fetch(`http://localhost:3000/rover/missionDetails?selectedRover=${selectedRover}`)
+                .then(res => res.json())//turn response to json
+                .then(detailsObject => {
+                    console.log(`details objetc`, detailsObject);
+                    ret = buildRoverDetailsDisplay(ret, detailsObject);
+
+                    draw(ret);
+
+                })
+
+            break;
+
+    }
+    return undefined;
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
+const Immutable = require('immutable')
 
 const app = express()
 const port = 3000
@@ -11,7 +12,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', express.static(path.join(__dirname, '../public')))
+let cache = Immutable.Map(); //store old queries for quick reply needs to be mutable to add values with immutable api
 
+
+function updateCache(queryParam, jsonReply) {
+    cache = cache.set(queryParam, jsonReply); //immutable cache replaced
+}
+function getCache(cachedName) {
+    return cache.get(cachedName);
+}
+
+/**
+ * name to store the response as, maxsol changes when new pics available
+ * @param {} url 
+ * @param {*} maxSol 
+ */
+function buildCacheName(url, maxSol) {
+    return `${maxSol}_${url}`;
+}
+
+
+function addToImageArray(toAddFrom, arr) {
+    toAddFrom.forEach(element => {
+        if (arr.length < 25) {
+            //only process element if it will be added
+            arr.push({ img_src: element.img_src, earth_date: element.earth_date })
+        }
+    });
+}
 // your API calls
 
 // example API call
@@ -25,4 +53,81 @@ app.get('/apod', async (req, res) => {
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/rover/latestPics', async (req, res) => {
+    // console.log(`req query`,req[`originalUrl`]);
+    const roverName = req.query[`selectedRover`];
+    try {
+        const manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json());
+        // console.log(`manifest`, manifest);
+        // console.log(`manifest`,manifest.photo_manifest.photos[0]);
+        const maxSol = manifest.photo_manifest.max_sol; //store latest sol and cache accordingly
+        const total = manifest.photo_manifest.total_photos;
+        const cacheName = buildCacheName(req[`originalUrl`], maxSol);
+        const photosArray = manifest.photo_manifest.photos;
+
+        let images = getCache(cacheName); //array of image urls
+        if (!images) {
+            images = [];
+            //if already cached return results else fetch 25 images and cache
+            //get ~ 25 images for page using earth days from manifest
+            let ArrInd = 0;
+            while (images.length < 25 && images.length < total) {
+                const next = photosArray[photosArray.length - 1 - ArrInd]; //get from latest pictures
+                const date = next.earth_date;
+
+                const imageTemp = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?earth_date=${date}&page=1&api_key=${process.env.API_KEY}`)
+                    .then(res => res.json());
+                addToImageArray(imageTemp.photos, images);
+
+                ArrInd++;
+            }
+            // console.log(`full array`, images);
+            updateCache(cacheName, images);
+
+        }
+        else {
+            console.log(`got cached data`, images);
+        }
+        res.send({ images });
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.get('/rover/missionDetails', async (req, res) => {
+    const roverName = req.query[`selectedRover`];
+    try {
+        const cacheName = buildCacheName(req[`originalUrl`], `undefined`);
+        let dets = getCache(cacheName); //array of image urls
+        if (!dets) {
+
+            const manifest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${roverName}?api_key=${process.env.API_KEY}`)
+                .then(res => res.json());
+
+            dets = { ...manifest.photo_manifest };
+            delete dets.photos;
+            // console.log(`full array`, dets);
+            updateCache(cacheName, dets);
+
+        }
+        else {
+            console.log(`got cached details`, dets);
+        }
+        res.send(dets);
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+    // listening for load event because page should load before any JS is called
+    try {
+        document.write("<script src='" + Immutable + "' data-main='/client.js'></script>");
+
+    } catch (exception) {
+
+    }
+
+})
